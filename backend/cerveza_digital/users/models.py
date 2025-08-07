75d7b8c3e62cd3a5deeedb20046a13f8841168f2
sistemas-c0fra <cooperativafrigorifica@gmail.com>
2025-08-07 09:10:51 -0300

    fix small bugs

--- backend/cerveza_digital/users/models.py
+++ backend/cerveza_digital/users/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
+    username = models.CharField(max_length=150, unique=False, blank=True, null=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
